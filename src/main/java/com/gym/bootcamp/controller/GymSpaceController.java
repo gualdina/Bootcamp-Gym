2280166651099739fe76cea2aef975cdbbdbfe30
Sofia <anaspa2006@hotmail.com>
2021-10-19 15:45:32 +0100

    Created Repository,Service and Controller of GymSpaces

---
+++ src/main/java/com/gym/bootcamp/controller/GymSpaceController.java
@@ -0,0 +1,24 @@
+package com.gym.bootcamp.controller;
+
+import com.gym.bootcamp.service.GymSpaceService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Validated
+@RestController
+public class GymSpaceController {
+private final GymSpaceService gymSpaceService;
+    public GymSpaceController(GymSpaceService gymSpaceService) {
+        this.gymSpaceService = gymSpaceService;
+    }
+    //Get all spaces
+    @GetMapping(value = "/space")
+    public List<GymSpace> getGymSpaceList() {
+        return gymSpaceService.findAll();
+    }
+  //
+
+}
